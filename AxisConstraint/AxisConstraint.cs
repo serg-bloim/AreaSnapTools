e019ff952831fbc8275ed204a9f2a7a266260645
serg.bloim <serg.bloim@gmail.com>
2026-08-11 18:21:36 -0400

    init

---
+++ AxisConstraint/AxisConstraint.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using HarmonyLib;
+using Mafi;
+using Mafi.Collections;
+using Mafi.Core.Console;
+using Mafi.Core.Game;
+using Mafi.Core.Mods;
+using Mafi.Core.Prototypes;
+
+namespace AxisConstraint;
+
+public sealed class AxisConstraint : IMod
+{
+    public const string HARMONY_PATCH_CATEGORY = "AxisConstraint";
+    private readonly Harmony harm = new("AxisConstraint");
+
+    // Mod constructor will be called on mod loading.
+    public AxisConstraint(ModManifest manifest) : base()
+    {
+        Manifest = manifest;
+        JsonConfig = new ModJsonConfig(this);
+    }
+
+
+    public void RegisterPrototypes(ProtoRegistrator registrator)
+    {
+        Log.Info("AxisConstraint: applying patches");
+        try
+        {
+            harm.UnpatchCategory(HARMONY_PATCH_CATEGORY);
+            harm.PatchCategory(HARMONY_PATCH_CATEGORY);
+            Log.Info("AxisConstraint: patching complete");
+        }
+        catch (Exception e)
+        {
+            Log.Exception(e, "AxisConstraint: failed to patch");
+        }
+    }
+
+    public void RegisterDependencies(DependencyResolverBuilder depBuilder, ProtosDb protosDb, bool gameWasLoaded)
+    {
+    }
+
+    public void EarlyInit(DependencyResolver resolver)
+    {
+        Log.Info($"AxisConstraint: v2");
+    }
+
+    public void Initialize(DependencyResolver resolver, bool gameWasLoaded)
+    {
+        Log.Info($"AxisConstraint: v1");
+        PatchBattleShip.console = resolver.Resolve<IGameConsole>();
+    }
+
+    public void MigrateJsonConfig(VersionSlim savedVersion, Dict<string, object> savedValues)
+    {
+    }
+
+    public ModManifest Manifest { get; }
+    public bool IsUiOnly { get; }
+    public Option<IConfig> ModConfig { get; }
+    public ModJsonConfig JsonConfig { get; }
+
+    public void Dispose()
+    {
+    }
+}
---
+++ AxisConstraint/Patches.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using Mafi;
+using Mafi.Collections;
+using Mafi.Core;
+using Mafi.Core.Console;
+using Mafi.Core.Entities.Static;
+using Mafi.Core.Products;
+using Mafi.Core.World;
+
+namespace AxisConstraint;
+[HarmonyPatchCategory(AxisConstraint.HARMONY_PATCH_CATEGORY)]
+[HarmonyPatch(typeof(BattleShip))]
+[HarmonyPatch(nameof(BattleShip.TryUnloadCargo))]
+public class PatchBattleShip
+{
+    public static IGameConsole console;
+
+    private static MethodInfo removeBufferIfEmpty =
+        AccessTools.Method(typeof(BattleShip), "removeBufferIfEmpty");
+    static bool Prefix(
+        ref BattleShip __instance,
+        ref ProductQuantity __result,
+        Quantity maxQuantity,
+        IReadOnlySet<ProductProto> productsToSkip)
+    {
+        try
+        {
+            __result = MethodReplacement(__instance, maxQuantity, productsToSkip);
+            return false;
+        }
+        catch(Exception e)
+        {
+            Log.Exception(e, "Error during a method patch(Mafi.Core.World.BattleShip.TryUnloadCargo), rolling back to the original method.");
+            return true;
+        }
+    }
+
+    private static ProductQuantity MethodReplacement(BattleShip battleShip, Quantity maxQuantity, IReadOnlySet<ProductProto> productsToSkip)
+    {
+        {
+            IProductBuffer buffer;
+            try
+            {
+                buffer = battleShip.Cargo.Values
+                    .Where(pb => !productsToSkip.Contains(pb.Product))
+                    .MinElement(pb => pb.Quantity);
+            }
+            catch (Exception e)
+            {
+                return ProductQuantity.None;
+            }
+
+            Quantity quantity = buffer.RemoveAsMuchAs(maxQuantity);
+            removeBufferIfEmpty.Invoke(battleShip, [buffer]);
+            if (quantity.IsPositive)
+                return new ProductQuantity(buffer.Product, quantity);
+            throw new Exception("Patched method could not find a positive quantity. Let the original method run");
+        }
+    }
+}
